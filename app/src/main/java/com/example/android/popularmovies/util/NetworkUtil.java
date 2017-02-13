90983d73cc1dcaa347bd52935896d8f93f5e8151
agomez40 <lagomez40@gmail.com>
2017-02-12 22:17:22 -0600

    feat(): add some network utils, and reusable API call methods

--- app/src/main/java/com/example/android/popularmovies/util/NetworkUtil.java
+++ app/src/main/java/com/example/android/popularmovies/util/NetworkUtil.java
@@ -18,6 +18,20 @@ package com.example.android.popularmovies.util;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Basic network utils
@@ -27,6 +41,76 @@ import android.net.NetworkInfo;
  * @since 1.0.0 2017/02/09
  */
 public class NetworkUtil {
+    /**
+     * The movie database base API URI
+     */
+    private static final String MOVIE_DATABASE_BASE_URL =
+            "https://api.themoviedb.org/3/movies/";
+
+    /**
+     * https://developers.themoviedb.org/3/movies/get-top-rated-movies
+     */
+    public static final String GET_TOP_RATED_MOVIES = "top_rated";
+
+    /**
+     * https://developers.themoviedb.org/3/movies/get-popular-movies
+     */
+    public static final String GET_POPULAR_MOVIES = "popular";
+
+    /**
+     * https://developers.themoviedb.org/3/movies/get-latest-movie
+     */
+    public static final String GET_LATEST_MOVIES = "latest";
+
+    /**
+     * Builds the URL used to query The Movie Database.
+     *
+     * @param path   The keyword for the API method or resource.
+     * @param params The query string parameters
+     * @return The URL to use to query the Movie Database API.
+     */
+    public static URL buildUrl(String path, Map<String, String> params) throws MalformedURLException {
+        Uri.Builder builder = Uri.parse(MOVIE_DATABASE_BASE_URL).buildUpon();
+        builder.appendPath(path);
+
+        if (params != null && !params.isEmpty()) {
+            // Append the params as query string
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                builder.appendQueryParameter(entry.getKey(), entry.getValue());
+                Log.d(NetworkUtil.class.getSimpleName(), "Added query Key = " + entry.getKey() + ", Value = " + entry.getValue());
+            }
+        }
+
+        return new URL(builder.build().toString());
+    }
+
+    /**
+     * Performs an HTTP GET using the provided url
+     *
+     * @param url the url
+     * @return the API response as a {@link JSONObject}
+     * @throws IOException   if the request could not be executed due to cancellation, a connectivity
+     *                       problem or timeout. Because networks can fail during an exchange, it is possible that the
+     *                       remote server accepted the request before the failure.
+     * @throws JSONException if the parse fails or doesn't yield a {@code
+     *                       JSONObject}.
+     * @see OkHttpClient
+     * @since 1.0.0 2017/02/12
+     */
+    public static JSONObject queryMovieDatabaseAPI(URL url) throws IOException, JSONException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        JSONObject jsonResponse = new JSONObject(response.body().toString());
+
+        // Close the stream, or it will leak memory
+        response.close();
+
+        return jsonResponse;
+    }
 
     /**
      * Checks the device's network connection and availability
