c8affacf294a5ef1902066253bd98e7c1b9bbcbb
agomez40 <lagomez40@gmail.com>
2017-04-02 19:21:49 -0500

    chore(): remove unused resources

--- app/src/main/java/com/example/android/popularmovies/ui/base/MovieTrailerAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/ui/base/MovieTrailerAdapter.java
@@ -1,10 +1,41 @@
 package com.example.android.popularmovies.ui.base;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MovieTrailerAdapter extends RecyclerView.Adapter<MovieTrailerAdapter.TrailersViewHolder>{
+import com.example.android.popularmovies.data.model.Video;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class MovieTrailerAdapter extends RecyclerView.Adapter<MovieTrailerAdapter.TrailersViewHolder> {
+    /**
+     * Class Logging tag
+     */
+    private static final String TAG = MovieTrailerAdapter.class.getSimpleName();
+    private static final String YOUTUBE_IMG_URL = "";
+
+    /**
+     * The data collection for the adapter
+     */
+    private List<Video> mVideos;
+
+    /**
+     * The container context
+     */
+    private Context mContext;
+
+    /**
+     * @param context
+     */
+    public MovieTrailerAdapter(Context context) {
+        this.mContext = context;
+    }
+
     @Override
     public MovieTrailerAdapter.TrailersViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
@@ -15,11 +46,25 @@ public class MovieTrailerAdapter extends RecyclerView.Adapter<MovieTrailerAdapte
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getItemCount() {
-        return 0;
+        return mVideos != null ? mVideos.size() : 0;
     }
 
+    /**
+     * @param videos The videos to set
+     */
+    public void setVideos(List<Video> videos) {
+        this.mVideos = videos;
+        notifyDataSetChanged();
+    }
+
+    /**
+     *
+     */
     class TrailersViewHolder extends RecyclerView.ViewHolder {
         public TrailersViewHolder(View itemView) {
             super(itemView);
--- app/src/main/java/com/example/android/popularmovies/ui/movies/MoviesActivity.java
+++ app/src/main/java/com/example/android/popularmovies/ui/movies/MoviesActivity.java
@@ -135,24 +135,6 @@ public class MoviesActivity extends BaseActivity implements MoviesAdapter.MovieI
             getSupportActionBar().setTitle(R.string.app_name);
         }
 
-        /*if (findViewById(R.id.weather_detail_container) != null) {
-            // The detail container view will be present only in the large-screen layouts
-            // (res/layout-sw600dp). If this view is present, then the activity should be
-            // in two-pane mode.
-            mTwoPane = true;
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            if (savedInstanceState == null) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.weather_detail_container, new DetailFragment(), DETAILFRAGMENT_TAG)
-                        .commit();
-            }
-        } else {
-            mTwoPane = false;
-            getSupportActionBar().setElevation(0f);
-        }*/
-
         mEvPopularMovies.setErrorViewListener(this);
 
         // Config the grid
