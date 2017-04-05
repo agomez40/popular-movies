7c5624be73680308f539f561f92edb3d52a90249
agomez40 <lagomez40@gmail.com>
2017-04-05 10:40:59 -0500

    feat(): add CustomChromeTabs to open the full review

--- app/src/main/java/com/example/android/popularmovies/ui/base/ReviewsAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/ui/base/ReviewsAdapter.java
@@ -1,6 +1,24 @@
+/*
+ * Copyright 2017 Luis Alberto Gómez Rodríguez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.android.popularmovies.ui.base;
 
 import android.content.Context;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -143,8 +161,11 @@ public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ReviewsV
          */
         @Override
         public void onClick(View v) {
-            // TODO open the url on the browser using an intent
-            review.url();
+            // open the url on the browser or custom tabs using an intent
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            builder.setToolbarColor(v.getResources().getColor(R.color.colorPrimary));
+            CustomTabsIntent customTabsIntent = builder.build();
+            customTabsIntent.launchUrl(v.getContext(), Uri.parse(review.url()));
         }
 
         /**
